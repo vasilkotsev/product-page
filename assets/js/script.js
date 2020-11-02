4d2c59c32fd8a1c64491bf40e8c936c0752d647e
Vasil Kotsev <vasil.kotsev@gmail.com>
2020-11-02 13:24:02 +0200

    adds functionalities refactoring and some styles changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,11 @@ const item = {
   title: "Дамска чанта PINKO",
   model: "Love Mini Puff 1",
   color: "Черен",
-  price: 640.00,
-  mainImageSrc: "/assets/images/picture-1.jpg",
+  price: 640.0,
+  mainImageSrc: "assets/images/picture-1.jpg",
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  
-  // Adding some mocked data dynamically to the page
-  const { title, model, color, price, mainImageSrc } = item;
-
-  let productTitle = document.querySelector(
-    "#main-product-view .product-title"
-  );
-  let productSubtitle = document.querySelector(
-    "#main-product-view .product-subtitle"
-  );
-  let productPriceParagraph = document.querySelector(
-    ".product-shop-details .product-price"
-  );
-  let productMainImage = document.querySelector(".main-img-holder .main-img");
-
-  productTitle.innerHTML = title;
-  productSubtitle.innerHTML = model;
-  productPriceParagraph.innerHTML = `${price.toFixed(2)} лв.`;
-  productMainImage.src = mainImageSrc;
-
   // Hamburger menu functionality
   let menu = document.querySelector(".menu");
   let logo = document.getElementById("logo");
@@ -54,53 +34,98 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  // Adding some mocked data dynamically to the page
+  const { title, model, color, price, mainImageSrc } = item;
+
+  let productTitle = document.querySelector(
+    "#main-product-view .product-title"
+  );
+  let productSubtitle = document.querySelector(
+    "#main-product-view .product-subtitle"
+  );
+  let productPriceParagraph = document.querySelector(
+    ".product-shop-details .product-price"
+  );
+  let productMainImage = document.querySelector(".main-img-holder .main-img");
+
+  productTitle.innerHTML = title;
+  productSubtitle.innerHTML = model;
+  productPriceParagraph.innerHTML = `${price.toFixed(2)} лв.`;
+  productMainImage.src = mainImageSrc;
+
+  createPopupModalCheckout();
+
   // Popup Modal checkout functionality
-  let addCartBtn = document.querySelector(
-    ".product-shop-details-box .cart-add-btn"
-  );
-  let popupModal = document.querySelector(".popup-modal");
-  let closePopupBtn = document.querySelector(".popup-close-btn");
-  let popupProductTitle = document.querySelector(
-    ".popup-modal .popup-product-title"
-  );
-  let popupProductSubtitle = document.querySelector(
-    ".popup-modal .popup-product-subtitle"
-  );
-  let popupProductColorParagraph = document.querySelector(
-    ".popup-modal .popup-product-color"
-  );
-  let popupProductPriceParagraph = document.querySelector(
-    ".popup-modal .popup-product-price"
-  );
-  let popupProductMainImage = document.querySelector(
-    ".popup-modal .popup-product-img"
-  );
+  function createPopupModalCheckout() {
+    // Adding some mocked data and creating DOM elements dynamically
+    let addCartBtn = document.querySelector(
+      ".product-shop-details-box .cart-add-btn"
+    );
+    let popupModal = document.querySelector(".popup-modal");
+    let closePopupBtn = document.querySelector(".popup-close-btn");
 
-  addCartBtn.addEventListener("click", () => {
-    popupModal.classList.add("show-popup");
-    document.body.style.overflow = "hidden";
-    // // Adding some mocked data dynamically to the popup
+    let popupProductTitle = document.createElement("h3");
+    popupProductTitle.className = "popup-product-title";
     popupProductTitle.innerHTML = title;
+
+    let popupProductSubtitle = document.createElement("p");
+    popupProductSubtitle.className = "popup-product-subtitle";
     popupProductSubtitle.innerHTML = model;
+
+    let popupProductColorParagraph = document.createElement("p");
+    popupProductColorParagraph.className = "popup-product-color";
     popupProductColorParagraph.innerHTML = `Цвят: ${color}`;
+
+    let popupProductPriceParagraph = document.createElement("p");
+    popupProductPriceParagraph.className = "popup-product-price";
     popupProductPriceParagraph.innerHTML = `${price.toFixed(2)} лв.`;
-    popupProductMainImage.src = mainImageSrc;
-  });
 
-  closePopupBtn.addEventListener("click", (e) => {
-    e.stopPropagation();
-    popupModal.classList.remove("show-popup");
-    document.body.style.overflow = "auto";
-  });
+    let popupProductInfoDiv = document.createElement("div");
+    popupProductInfoDiv.className = "popup-product-info";
 
-  popupModal.addEventListener("click", (e) => {
-    if (e.target === popupModal) {
+    popupProductInfoDiv.append(
+      popupProductTitle,
+      popupProductSubtitle,
+      popupProductColorParagraph,
+      popupProductPriceParagraph
+    );
+
+    let popupProductImage = document.createElement("img");
+    popupProductImage.src = mainImageSrc;
+    popupProductImage.className = "popup-product-img";
+
+    let popupProductImageHolderDiv = document.createElement("div");
+    popupProductImageHolderDiv.className = "popup-img-holder";
+    popupProductImageHolderDiv.appendChild(popupProductImage);
+
+    let popupProductRowDiv = document.createElement("div");
+    popupProductRowDiv.className = "popup-product-row";
+    popupProductRowDiv.append(popupProductImageHolderDiv, popupProductInfoDiv);
+
+    addCartBtn.addEventListener("click", () => {
+      popupModal.classList.add("show-popup");
+      document.querySelector(".popup-product-cart").prepend(popupProductRowDiv);
+      document.body.style.overflow = "hidden";
+    });
+
+    closePopupBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      popupProductRowDiv.remove();
       popupModal.classList.remove("show-popup");
       document.body.style.overflow = "auto";
-    }
-  });
+    });
+
+    popupModal.addEventListener("click", (e) => {
+      if (e.target === popupModal) {
+        popupProductRowDiv.remove();
+        popupModal.classList.remove("show-popup");
+        document.body.style.overflow = "auto";
+      }
+    });
+  }
 
   // Carousel Image Gallery Modal Functionality
+
   let modalImagelGallery = document.getElementById("modal-img-gallery");
   let modalImageCloseBtn = document.querySelector(
     "#modal-img-gallery .image-modal-close-btn"
